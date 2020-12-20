c8437a0d7c9e330aaca14d5bbfe6c7aa997a71b1
Артём <komediantto@yandex.ru>
2020-12-20 18:35:29 +0200

    After review

--- homework.py
+++ homework.py
@@ -2,6 +2,7 @@ import datetime as dt
 
 
 class Calculator:
+
     def __init__(self, limit):
         self.records = []
         self.limit = abs(limit)
@@ -10,21 +11,17 @@ class Calculator:
         self.records.append(record)
 
     def get_today_stats(self):
-        sum = 0
         today = dt.date.today()
-        for record in self.records:
-            if record.date == today:
-                sum += record.amount
-            else:
-                pass
-        print(f'За сегодня вышло {sum}')
-        return sum
+        spend_money = [record.amount for record in self.records
+                      if record.date == today]        
+        sum_today = sum(spend_money,)
+        return sum_today
 
     def get_week_stats(self):
         week_sum = 0
+        today = dt.date.today()
+        week = today - dt.timedelta(days=7)
         for record in self.records:
-            today = dt.date.today()
-            week = today - dt.timedelta(days=8)
             if week <= record.date <= today:
                 week_sum += record.amount
         return week_sum
@@ -32,30 +29,22 @@ class Calculator:
 
 class Record:
     def __init__(self, amount, comment, date=None):
-        self.amount = abs(amount)
+        self.amount = amount
         self.comment = comment
         self.date = date
-        if date == None:
+        if date is None:
             self.date = dt.date.today()
         else:
             self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()    
 
-    def show(self):
-        print(f'Сумма: {self.amount}, Цель: {self.comment}, Дата: {self.date}')
-    pass
-
 class CaloriesCalculator(Calculator):
-    def __init__(self, limit):
-        super().__init__(limit)
-
     def get_calories_remained(self):
-        today_cal = Calculator.get_today_stats(self)
+        today_cal = self.get_today_stats()
         remain = self.limit - today_cal
         if today_cal < self.limit:    
             return ('Сегодня можно съесть что-нибудь ещё, '
              f'но с общей калорийностью не более {remain} кКал')
-        else:
-            return('Хватит есть!')
+        return('Хватит есть!')
 
 
 class CashCalculator(Calculator):
@@ -63,35 +52,30 @@ class CashCalculator(Calculator):
     EURO_RATE = 89.04
     RUB_RATE = 1
 
-    def __init__(self, limit):
-        super().__init__(limit)
-
     def get_today_cash_remained(self, currency):
-        today_sum = Calculator.get_today_stats(self)
-        if currency == 'eur':
-            rate = self.EURO_RATE
-            money_name = 'Euro'
-        elif currency == 'usd':
-            rate = self.USD_RATE
-            money_name = "USD"   
-        elif currency == 'rub':
-            rate = self.RUB_RATE
-            money_name = 'руб'    
+        today_sum = self.get_today_stats()
+        dict_of_cur = {
+            'eur' : (self.EURO_RATE, 'Euro'),
+            'usd' : (self.USD_RATE, 'USD'),
+            'rub' : (self.RUB_RATE, 'руб')
+        }
+        if currency in dict_of_cur.keys():
+            rate = dict_of_cur[currency][0]
+            money_name = dict_of_cur[currency][1]
         else:
             print('Я не знаю такой валюты')
 
         if today_sum < self.limit:
             remains = round((self.limit - today_sum)/rate, 2)    
-            return (f'На сегодня осталось {(remains)} {money_name}')
+            return (f'На сегодня осталось {remains} {money_name}')
         elif today_sum == self.limit:
             return ('Денег нет, держись')
-        else:
-            remains = abs(round((self.limit - today_sum)/rate, 2))
-            return(f'Денег нет, держись: твой долг - {(remains)} {money_name}')         
+        remains = abs(round((self.limit - today_sum)/rate, 2))
+        return(f'Денег нет, держись: твой долг - {remains} {money_name}')         
 
-
-wallet = CashCalculator(5000)
-wallet.add_record(Record(5000, 'LoL'))
-print(wallet.get_today_cash_remained('rub'))
+if __name__ == "__main__":
+    wallet = CashCalculator(5000)
+    wallet.add_record(Record(3, 'LoL'))
+    print(wallet.get_today_cash_remained('eur'))
 
 
